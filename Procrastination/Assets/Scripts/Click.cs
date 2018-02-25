c89d29451bf65d247f5f7eef019da4245a295292
Ltrmex <maciekmajchrzak23@wp.pl>
2018-02-25 19:43:40 +0000

    created basic canvas with simple functionality using buttons

---
+++ Procrastination/Assets/Scripts/Click.cs
@@ -0,0 +1,27 @@
+﻿/*
+Name: Maciej Majchrzak
+G-Number: G00332746
+Group: B
+Code Reference: https://www.youtube.com/watch?v=sTJyJvHZZ1I
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+    //  Variables
+    public UnityEngine.UI.Text goldDisplay;
+    public UnityEngine.UI.Text gpc;
+    public float gold = 0.0f;
+    public int goldPerClick = 1;
+
+    private void Update() {
+        goldDisplay.text = "Gold: " + gold;
+        gpc.text = "GPC: " + goldPerClick;
+    }   //  Update()
+
+    public void Clicked() {
+        gold += goldPerClick;
+    }   //  Clicked()
+}   //  Click
---
+++ Procrastination/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,38 @@
+﻿/*
+Name: Maciej Majchrzak
+G-Number: G00332746
+Group: B
+Code Reference: https://www.youtube.com/watch?v=sTJyJvHZZ1I
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour {
+    //  Variables
+    public Click click;
+    public UnityEngine.UI.Text itemInfo;
+    public float cost;
+    public int count = 0;
+    public int clickPower;
+    public string itemName;
+    private float _newCost;
+
+    private void Update() {
+        itemInfo.text = itemName + "\nCost: " + cost + "\nPower: " + clickPower;
+    }   //  Update()
+
+    public void PurchasedUpgrade() {
+        if (click.gold >= cost) {
+            click.gold -= cost;
+            ++count;
+            click.goldPerClick += clickPower;
+
+            // Increase the price
+            cost = Mathf.Round(cost * 1.15f);
+            _newCost = Mathf.Pow(cost, _newCost = cost);
+        }   //  if
+    }   //  PurchasedUpgrade()
+
+}   //  UpgradeManager
