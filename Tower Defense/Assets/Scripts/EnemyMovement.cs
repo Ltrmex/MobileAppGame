7359181aa4489a00f26e88d27ce503016114aae6
Ltrmex <maciekmajchrzak23@wp.pl>
2018-03-21 23:35:52 +0000

    enemy movement, enemy spawner

---
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public float speed;
+    private Transform target;
+    private int wavepointIndex;
+
+	// Use this for initialization
+	void Start () {
+        speed = 10f;
+        wavepointIndex = 0;
+        target = Waypoints.waypoints[0];
+    }   //  Start ()
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+            GetNext();
+    }   //  Update ()
+
+    private void GetNext() {
+        if (wavepointIndex >= Waypoints.waypoints.Length - 1) { 
+            Destroy(gameObject);
+            return;
+        }   //  if
+
+        ++wavepointIndex;
+        target = Waypoints.waypoints[wavepointIndex];
+    }   //  GetNext()
+}
---
+++ Tower Defense/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public GameObject enemy;
+    public float interval;
+
+    void Start() {
+        InvokeRepeating("Spawn", interval, interval);
+    }   //  Start()
+
+    private void Spawn() {
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+    }   //  Spawn()
+}
