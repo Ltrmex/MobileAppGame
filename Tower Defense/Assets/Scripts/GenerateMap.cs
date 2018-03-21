52f1d03473b2470a04523f2e25d4b1c54bc4673b
Ltrmex <maciekmajchrzak23@wp.pl>
2018-03-21 22:15:46 +0000

    created map layout

---
+++ Tower Defense/Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        GameObject prefab = Resources.Load("Nodes") as GameObject;
+
+        for (int i = 0; i < 15; i++) {
+            GameObject go = Instantiate(prefab) as GameObject;
+            go.transform.position = new Vector3(0, 0, i*5);
+        }   //  for
+    }   //  Start ()
+
+}   //  GenerateMap
