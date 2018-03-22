d117e65a4560405be021a7c6afd346c62db28756
Ltrmex <maciekmajchrzak23@wp.pl>
2018-03-22 18:38:20 +0000

    turret follow, turret shoot, particle system

---
+++ Tower Defense/Assets/Scripts/Turret/Bullet.cs
@@ -0,0 +1,50 @@
+﻿/*
+Name: Maciej Majchrzak
+Code Adapted From: https://github.com/Brackeys/Tower-Defense-Tutorial
+*/
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    //  Variables
+    private Transform target;   //  enemy target
+    public float speed = 70f;   //  speed of bullets
+    public GameObject impactEffect; //  when bullet hits the enemy
+
+    //  Set the enemy target
+    public void Seek(Transform _target) {
+        target = _target;
+    }   //  Seek
+
+    // Update is called once per frame
+    void Update() {
+
+        //  If no target
+        if (target == null) {
+            Destroy(gameObject);    //  destroy the bullet
+            return; //  return - destroy might take a while to finish off
+        }   //  if
+
+        Vector3 direction = target.position - transform.position;   //  find direction for bullet to travel
+        float distance = speed * Time.deltaTime;   //  how much move per frame
+
+        //  Check if length of direction vector is less than or equal to distance is moved this frame
+        if (direction.magnitude <= distance) {
+            HitTarget();    //  then target was hit
+            return; //  if enemy hit then return
+        }   //  if
+
+        //  Move the bullet in proper direction
+        transform.Translate(direction.normalized * distance, Space.World);  //   move at constant speed
+
+    }   //  Update()
+
+    //  When target is hit
+    void HitTarget() {
+        //  Create instance of the particle effect
+        GameObject effect = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 2f);    //  destroy the effect after two seconds
+
+        Destroy(target.gameObject); //  destroy target
+        Destroy(gameObject);    //  destroy bullet
+    }   //  HitTarget()
+}   //  Bullet
---
+++ Tower Defense/Assets/Scripts/Turret/Turret.cs
@@ -0,0 +1,92 @@
+﻿/*
+Name: Maciej Majchrzak
+Code Adapted From: https://github.com/Brackeys/Tower-Defense-Tutorial
+*/
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+    //  Variables
+    private Transform target;   //  current target - enemy
+
+    [Header("Attributes")]
+    public float range = 15f;   //  range of the shooting & following
+    public float fireRate = 1f; //  fire one bullet each second
+    private float fireCountdown = 0f;   //  shooting countdown
+
+    [Header("Unity Setup Fields")]
+    public string enemyTag = "Enemy";   //  tag reference
+    public Transform gun;  //  reference to part which will rotate
+    public float turnSpeed = 10f;   //  how fast turret rotates
+    public GameObject bulletPrefab; //  bullet reference
+    public Transform firePoint; //  reference to point at which bullet spawns
+
+    // Use this for initialization
+    void Start() {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);  //  repeat updating the target at a interval
+    }   //   Start()
+
+    //  Update target - which enemy turret will follow
+    void UpdateTarget() {
+        //  Variables
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag); //  get reference to all of the enemies
+        float shortestDistance = Mathf.Infinity;    //  shortest distance to enemy
+        GameObject nearestEnemy = null; //  nearest enemy that was found
+
+        //  Loop through each enemy
+        foreach (GameObject enemy in enemies) {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position); //  variable to store distance between the turret and enemy
+
+            //  Check if enemy's location is the shortest 
+            if (distanceToEnemy < shortestDistance) {
+                shortestDistance = distanceToEnemy; //  set new shortestDistance
+                nearestEnemy = enemy;   //  mark this enemy as the nearest one
+            }   //  if
+        }   //  foreach
+
+        //  If found enemy and within range
+        if (nearestEnemy != null && shortestDistance <= range)
+            target = nearestEnemy.transform;    //  set this enemy as a target
+        else
+            target = null;  //  else no target
+
+    }   //  UpdateTarget()
+
+    // Update is called once per frame
+    void Update() {
+        if (target == null) //  if no target then return
+            return;
+
+        //  Target lock on
+        Vector3 direction = target.position - transform.position;   //  direction to point at, direction from one point to another point
+        Quaternion lookRotation = Quaternion.LookRotation(direction);   //  turn rotation
+
+        //  Rotation - smooth transition over time
+        Vector3 rotation = Quaternion.Lerp(gun.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+
+        //  Rotate head of the turret
+        gun.rotation = Quaternion.Euler(0f, rotation.y, 0f);    //   around y axis
+
+        //  If time to shoot
+        if (fireCountdown <= 0f) {
+            Shoot();    //  then shoot
+            fireCountdown = 1f / fireRate;  //  set fireCountdown
+        }   //  if
+
+        fireCountdown -= Time.deltaTime;    //  countdown fireCountdown
+
+    }   //  Update()
+
+    void Shoot() {
+        //  Variables
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);    //  temporary reference to the bullet, create instance of bullet
+        Bullet bullet = bulletGO.GetComponent<Bullet>();    //  get bullet script
+
+        if (bullet != null) //  if bullet is not null
+            bullet.Seek(target);    //  seek enemy target
+    }   //  Shoot()
+
+    //  Display turret's range when selected
+    void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(transform.position, range);   //  draw sphere with range as radius
+    }   //  OnDrawGizmosSelected()
+}   //  Turret
