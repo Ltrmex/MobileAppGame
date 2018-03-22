eeaae372eb27dc9ff2d6044ddae99deb9db0c5c9
Ltrmex <maciekmajchrzak23@wp.pl>
2018-03-22 22:48:34 +0000

    turret placement

---
+++ Tower Defense/Assets/Scripts/Turret/Builder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour {
+    public static Builder instance; //  singleton
+    private GameObject build;
+    public GameObject prefab;
+
+    private void Awake() {
+        if(instance != null) {
+            Debug.Log("Already defined");
+            return;
+        }   //  if
+
+        instance = this;
+    }   //  Awake()
+
+    private void Start() {
+        build = prefab;
+    }   //  Start()
+
+    public GameObject GetTurret() {
+
+        return build;
+    }   //  GetTurret()
+
+}   //  Builder
---
+++ Tower Defense/Assets/Scripts/Turret/Placement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Placement : MonoBehaviour {
+    public Color startColor;
+    public Color hoverColor;
+    public Vector3 offset;
+    private new Renderer renderer;
+    private GameObject place;
+
+    private void Start() {
+        renderer = GetComponent<Renderer>();
+    }   //  Start()
+
+    private void OnMouseEnter() {
+        renderer.material.color = hoverColor;
+    }   //  OnMouseEnter()
+
+    private void OnMouseExit() {
+        renderer.material.color = startColor;
+    }   //  OnMouseExit()
+
+    private void OnMouseDown() {
+        if (place != null) {
+            Debug.Log("Already occupied");
+            return;
+        }   //  if
+
+        GameObject build = Builder.instance.GetTurret();
+        place = (GameObject)Instantiate(build, transform.position + offset,transform.rotation);
+
+    }   //  OnMouseDown()
+}   //  Placement
