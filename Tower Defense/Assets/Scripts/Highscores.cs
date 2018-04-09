8067692fa07f7be7e62dc76369c3464556f088f4
Ltrmex <maciekmajchrzak23@wp.pl>
2018-04-09 16:36:37 +0100

    highscores layout and setup

---
+++ Tower Defense/Assets/Scripts/Highscores.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Highscores : MonoBehaviour {
+    public User [] user;
+    int i = 0;
+
+    // Use this for initialization
+    void Start () {
+        while (i < user.Length) {
+            user[i].Name = "John " + (i + 1);
+            user[i].Difficulty = "Easy " + (i + 1);
+            user[i].Wave = (i + 1);
+            user[i].SetValues();
+            ++i;
+        }   //  for
+	}   //  Start()
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}   //  Highscores
---
+++ Tower Defense/Assets/Scripts/User.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.UI;
+
+[System.Serializable]
+public class User {
+    public Text nameDisplay;
+    public Text difficultyDisplay;
+    public Text waveDisplay;
+
+    private string name;
+    private string difficulty;
+    private int wave;
+
+    public string Name {
+        get { return name; }
+        set { name = value; }
+    }   //  Name
+
+    public string Difficulty {
+        get { return difficulty; }
+        set { difficulty = value; }
+    }   //  Difficulty
+
+    public int Wave {
+        get { return wave; }
+        set { wave = value; }
+    }   //  Wave
+
+    public void SetValues() {
+        nameDisplay.text = Name;
+        difficultyDisplay.text = Difficulty;
+        waveDisplay.text = Wave.ToString();
+    }   //  SetValues()
+}   //  User
